7adeb6a3f555c26beb536b0aaaf7746cbdaced7f
Meg <megangraf@mymail.mines.edu>
2018-10-04 20:56:45 -0600

    Changed Set instances to HashSets

--- ClueGame/src/experiment/IntBoard.java
+++ ClueGame/src/experiment/IntBoard.java
@@ -8,9 +8,11 @@ public class IntBoard {
 	private Map<BoardCell, Set<BoardCell>> adjMatrix;
 	private Set<BoardCell> visited;
 	private HashSet<BoardCell> targets;
-	private BoardCell[][] grid;
-	private Set<BoardCell> adjList;
+	private BoardCell[][] grid;	
 	private Set<BoardCell> allCells;
+	
+	int numRows = 4;		//These are up here so they can be accessed by the calcAdjacencies
+	int numColumns = 4;
 	
 	
 	public IntBoard(int numRows, int numColumns) {
@@ -23,15 +25,32 @@ public class IntBoard {
 		for (int i = 0; i < numRows; i++) {
 			for (int j = 0; j < numColumns; j++) {
 				BoardCell myCell = new BoardCell(i,j);
-				allCells.put(myCell);
-				myCell.calcAdjacencies();
+				allCells.add(myCell);
+				adjMatrix.put(myCell, calcAdjacencies(myCell));
 			}
 		}
 			
-		calcAdjacencies();
 	}
-	public Set calcAdjacencies(){
-		
+	public Set<BoardCell> calcAdjacencies(BoardCell myCell){
+		Set<BoardCell> adjList  = new HashSet<BoardCell>();		//I moved this to inside the method because it will be re-inititated for each cell
+		int myRow = myCell.row;
+		int myColumn = myCell.column;
+		if (myRow > 0) {
+			BoardCell up = new BoardCell(myRow - 1, myColumn);
+			adjList.add(up);
+		}
+		if (myRow < numRows - 1) {								// - 1 since we start counting from 0
+			BoardCell down = new BoardCell(myRow + 1, myColumn);
+			adjList.add(down);
+		}
+		if (myColumn > 0) {
+			BoardCell left = new BoardCell(myRow, myColumn - 1);
+			adjList.add(left);
+		}
+		if (myColumn < numColumns - 1) {
+			BoardCell right = new BoardCell(myRow, myColumn + 1);
+			adjList.add(right);
+		}		
 		return adjList;
 	}
 	public HashSet calcTargets(BoardCell startCell, int pathLength){
@@ -39,7 +58,7 @@ public class IntBoard {
 	}
 	
 	public Set<BoardCell> getAdjList(BoardCell cell) {
-		return adjList;
+		return adjMatrix.get(cell);								//get the matching list from the Map directly
 	}
 	public HashSet<BoardCell> getTargets() {
 		return targets;
