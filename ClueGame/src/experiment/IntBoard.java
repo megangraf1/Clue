d30555e093dd91a36219de99767e888d0aaf8090
Meg <megangraf@mymail.mines.edu>
2018-10-08 00:32:09 -0600

    CalcTargets completed

--- ClueGame/src/experiment/IntBoard.java
+++ ClueGame/src/experiment/IntBoard.java
@@ -2,15 +2,14 @@ package experiment;
 
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
 public class IntBoard {
-	public static Map<BoardCell, Set<BoardCell>> adjMatrix = new HashMap<BoardCell, Set<BoardCell>>();
+	public static Map<BoardCell, Set<BoardCell>> adjMatrix;
 	public Set<BoardCell> visited;
 	public BoardCell[][] grid;	
-	public Set<BoardCell> allCells;
+	public Set<BoardCell> targets = new HashSet<BoardCell>();
 
 	public static int numRows = 4;		//These are up here so they can be accessed by the calcAdjacencies
 	public static int numColumns = 4;
@@ -20,8 +19,8 @@ public class IntBoard {
 		super();
 		// need to allocate space for all the sets used in the tests
 		//use a size of zero so no exception errors
-		Set<BoardCell> allCells = new HashSet<BoardCell>();
-		BoardCell[][] grid = new BoardCell[numRows + 1][numColumns + 1];
+		adjMatrix = new HashMap<BoardCell, Set<BoardCell>>();
+		grid = new BoardCell[numRows + 1][numColumns + 1];
 
 		for (int i = 0; i <= numRows; i++) {
 			for (int j = 0; j <= numColumns; j++) {
@@ -35,7 +34,7 @@ public class IntBoard {
 	public void calcAdjacencies(){
 		for (int i = 0; i <= numRows; i++) {		//iterate thru grid instead
 			for (int j = 0; j <= numColumns; j++) {
-				Set<BoardCell> returnSet = new HashSet<BoardCell>();
+				Set<BoardCell> returnSet = new HashSet<BoardCell>();	//reinitialized for each cell
 				int cellX = i;
 				int cellY = j;
 				
@@ -54,40 +53,44 @@ public class IntBoard {
 				adjMatrix.put(grid[i][j], returnSet);
 			}
 		}
+		
 	}
 
-	/*
-	public Set<BoardCell> calcTargets(BoardCell startCell, int pathLength){
-		boolean moreSteps = true;
-		int numOfLoops = 0;											// how many times you've gone through
-		//calculate adjacencies as many times as pathlength, for each adjacent cell
-		HashSet<BoardCell> targets = new HashSet<BoardCell>();
-		Set<BoardCell> tempSet = new HashSet<BoardCell>();
-		Set<BoardCell> start = calcAdjacencies(startCell);
-
-		while (moreSteps == true) {
-			for (BoardCell thisCell : start) {						//go through the initial adjacents
-				tempSet = calcAdjacencies(thisCell);				//get the set of adjacents for each of those
-				for (BoardCell thatCell : tempSet) {				//go through each of *those* sets
-					targets.add(thatCell);							//add that cell to the new targets loop
-				}
-			} 
-			targets.removeAll(start);								//no backtracking allowed
-			numOfLoops++;
-			start.clear();
-			start.addAll(targets);
-			if (numOfLoops == pathLength) moreSteps = false;		//break out of the loop
+	
+	public void calcTargets(BoardCell thisCell, int pathLength){
+		Set<BoardCell> visited = new HashSet<BoardCell>();
+		Set<BoardCell> adjacentCells;
+		targets.clear();
+		visited.add(thisCell);
+		adjacentCells = adjMatrix.get(thisCell);
+		for (BoardCell adjCell : adjacentCells) {
+			if (visited.contains(adjCell)) {					//move to the next adjCell if you've already visited this one
+				continue;
+			}
+			if (pathLength == 1) {
+				targets.add(adjCell);
+			}
+			else {
+				calcTargets(adjCell, pathLength-1);
+			}
+		}		
+		
+		for (BoardCell t : targets) {							//remove internal visited cells
+			if (visited.contains(t)) {
+				targets.remove(t);
+			}
 		}
-		return start;
 	}
-	 */
+	
 	public Set<BoardCell> getAdjList(BoardCell cell) {
 		return adjMatrix.get(cell);								//get the matching list from the Map directly
 	}
-	/*public Set<BoardCell> getTargets(BoardCell startCell, int pathLength) {					 
-		Set<BoardCell> mySet = calcTargets(startCell, pathLength);
-		return mySet;
-	}*/
+	
+	public Set<BoardCell> getTargets(BoardCell startCell, int pathLength) {					 
+		calcTargets(startCell, pathLength);
+		return targets;
+	}
+	
 	public BoardCell getCell(int row, int column) {
 		BoardCell cell = new BoardCell(row,column);
 		//assigns a new cell to have a certain row and column
@@ -96,7 +99,7 @@ public class IntBoard {
 
 		return cell;
 	}
-	//TODO - visited
+
 	public static void main(String []args) {
 		IntBoard myBoard = new IntBoard(numRows,numColumns);
 	}
